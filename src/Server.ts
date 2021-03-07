b84d446b7a43ceb18783301d89ea8b34050b6f31
b.komazec <k.slobodan@yahoo.com>
2021-03-07 08:36:45 +0100

    Update version

--- src/Server.ts
+++ src/Server.ts
@@ -8,6 +8,9 @@ export default class Server {
     private serverResponse: http.ServerResponse
     private dataService: DataService;
 
+    /**
+     * Constructor
+     */
     constructor() {
         this.dataService = new DataService();
     }
